619695e00d6d0e41977993754600ab4539b12122
Costa <macosta@inf.ufpel.edu.br>
2018-02-20 00:34:08 -0300

    model: refactored class model

--- CG_UFPel/include/model.hpp
+++ CG_UFPel/include/model.hpp
@@ -5,14 +5,11 @@
 #include <stdlib.h>
 #include <vector>
 
-
 #include <mesh.hpp>
 #include <vboindexer.hpp>
 #include <controls.hpp>
 #include <shader.hpp>
 #include <texture.hpp>
-
-
 
 class Model{
 public:
@@ -37,40 +34,24 @@ public:
     
     void SetPvm();
     
-    //glm::mat4 GetProjectionMat();
-    //glm::mat4 GetViewMatrix();
     glm::mat4 GetModelMatrix();
     
-    //void SetProjectionMat(glm::mat4 PM);
-    //void SetViewMatrix(glm::mat4 VM);
     void SetModelMatrix(glm::mat4 MM);
     
 private:
     
-    //glm::mat4 ProjectionMatrix;
-    //glm::mat4 ViewMatrix;
     glm::mat4 ModelMatrix;
-    
-    
     GLuint VertexArrayID;
-    
     GLuint programID;
-    
     GLuint MatrixID;
     GLuint ViewMatrixID;
     GLuint ModelMatrixID;
-    
     GLuint Texture;
-    
     GLuint TextureID;
-    
     GLuint LightID;
-    
     glm::vec3 lightPos;
-    
+
 };
-
-
 
 #endif
 
--- CG_UFPel/sources/model.cpp
+++ CG_UFPel/sources/model.cpp
@@ -3,34 +3,19 @@
 Model::Model(){}
 
 Model::Model(char* vertex, char* fragment, char* textura){
-    
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
-    
-    // Create and compile our GLSL program from the shaders
     programID = LoadShaders(vertex, fragment);
-    
-    // Get a handle for our "MVP" uniform
     MatrixID      = glGetUniformLocation(programID, "MVP");
     ViewMatrixID  = glGetUniformLocation(programID, "V");
     ModelMatrixID = glGetUniformLocation(programID, "M");
-    
-    
-    // Load the texture
-//    Texture = loadDDS(textura);
     Texture = loadBMP_custom(textura);
-    
-    // Get a handle for our "myTextureSampler" uniform
     TextureID = glGetUniformLocation(programID, "myTextureSampler");
-    
-    
-    
 }
 
 Model::~Model(){}
 
 void Model::SetLight(){
-    // Get a handle for our "LightPosition" uniform
     glUseProgram(programID);
     LightID = glGetUniformLocation(programID, "LightPosition_worldspace");
 }
@@ -40,20 +25,17 @@ void Model::ShaderModel(){
 }
 
 void Model::SetDraw(glm::mat4 MVP, glm::mat4 ViewMatrix ){
-    // Send our transformation to the currently bound shader,
-    // in the "MVP" uniform
     glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
     glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
     glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
 }
 
 void Model::DrawModel(std::vector<unsigned short> inde){
-    // Draw the triangles !
     glDrawElements(
-                   GL_TRIANGLES,        // mode
-                   inde.size(),      // count
-                   GL_UNSIGNED_SHORT,   // type
-                   (void*)0             // element array buffer offset
+                   GL_TRIANGLES,
+                   inde.size(),
+                   GL_UNSIGNED_SHORT,
+                   (void*)0
                    );
 }
 
@@ -63,10 +45,8 @@ void Model::Light(){
 }
 
 void Model::TextureM(){
-    // Bind our texture in Texture Unit 0
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, Texture);
-    // Set our "myTextureSampler" sampler to user Texture Unit 0
     glUniform1i(TextureID, 0);
 }
 
@@ -76,32 +56,14 @@ void Model::CleanPtv(){
     glDeleteVertexArrays(1, &VertexArrayID);
 }
 
-
 void Model::SetPvm(){
-    //ProjectionMatrix = getProjectionMatrix();
-    //ViewMatrix       = getViewMatrix();
     ModelMatrix      = glm::mat4(1.0);
 }
-
-
-/*glm::mat4 Model::GetProjectionMat(){
- //return ProjectionMatrix;
- }
- 
- glm::mat4 Model::GetViewMatrix(){
- //return ViewMatrix;
- }*/
 
 glm::mat4 Model::GetModelMatrix(){
     return ModelMatrix;
 }
 
-/*void Model::SetProjectionMat(glm::mat4 PM){
- //ProjectionMatrix = PM;
- }
- void Model::SetViewMatrix(glm::mat4 VM){
- //ViewMatrix = VM;
- }*/
 void Model::SetModelMatrix(glm::mat4 MM){
     ModelMatrix = MM;
 }
